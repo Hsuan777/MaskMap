bea9ae2407407d059a6b91f57186f4270af40901
syuan0212@gmail.com <hsuan777>
2020-03-23 22:36:57 +0800

    修正__選擇縣市

--- js/masklist.js
+++ js/masklist.js
@@ -3,43 +3,54 @@ xhr.open('get', 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/
 xhr.send(null);
 xhr.onload = function () {
   var data = JSON.parse(xhr.responseText).features;
-//   var county = document.getElementById('county');
+  var county = document.getElementById('county');
+  var town = document.getElementById('town');
+
+  // 市 -> 縣
   var countyArray = [];
-//   var countyName =
-//   {
-//     "county": "",
-//   }
+  for (let i = 0; i < data.length; i++) {
+    if (countyArray.indexOf(data[i].properties.county) === -1 && (data[i].properties.county) !== "") {
+      countyArray.push(data[i].properties.county);
+    }
+  }
+  for (let i = 0; i < countyArray.length; i++) {
+    var option = document.createElement('option');
+    option.textContent = countyArray[i];
+    option.setAttribute('value', countyArray[i]);
+    county.appendChild(option);
+  }
 
-//   county.addEventListener('change', function (e) {
-//     // 指定縣市之所有藥局
-//     console.log(e.target.value);
 
-//     for (let i = 0; i < data.length; i++) {
-//       if (e.target.value === data[i].county) {
-//         console.log(data[i].county);
-//         countyName.county = data[i].county;
-//         countyArray.push(countyName.county);
-//       }
-//     }
-//   });
-  countyF();
-//   console.log(countyArray);
-  function countyF(){
-    // 市 -> 縣
-    var countyArray =[];
+
+  county.addEventListener('change', function (e) {
+    // 列出該縣市之所有藥局
+    var selectTown = document.querySelectorAll(".select__town");
+    for (i = 0; i < selectTown.length; i++) {
+      town.removeChild(selectTown[i]);
+    }
+    townF(e.target.value);
+
+
+  });
+
+
+  function townF(county) {
     // 區、鄉、鎮
-    var townArray =[];
-    for (let i = 0; i < data.length; i++){
-      
-      if (countyArray.indexOf(data[i].properties.county) === -1 && (data[i].properties.county) !== ""){
-        countyArray.push(data[i].properties.county);
-      }
-      if (townArray.indexOf(data[i].properties.town) === -1 && (data[i].properties.county) !== "" 
-      && data[i].properties.county === countyArray[8]){
+    var townArray = [];
+    for (let i = 0; i < data.length; i++) {
+      if (townArray.indexOf(data[i].properties.town) === -1 && (data[i].properties.county) !== ""
+        && data[i].properties.county === county) {
         townArray.push(data[i].properties.town);
       }
     }
-    console.log(countyArray);
+    for (let i = 0; i < townArray.length; i++) {
+      var option = document.createElement('option');
+      option.classList.add('select__town');
+      option.textContent = townArray[i];
+      option.setAttribute('value', townArray[i]);
+      town.appendChild(option);
+      // town.innerHTML = '<option value="">'+townArray[i]+'</option>';
+    }
     console.log(townArray);
     return;
   }
