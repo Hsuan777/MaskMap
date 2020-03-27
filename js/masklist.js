86d306ff8ef6347be0a2b33d312e9e3e7f56d820
Vic <Vic@gmail.com>
2020-03-27 16:43:27 +0800

    修正__分頁顯示功能

--- js/masklist.js
+++ js/masklist.js
@@ -16,7 +16,7 @@ xhr.onload = function () {
   // 預設
   // - 列出所有縣市
   countyF();
-  
+
   scrollFunction();
 
 
@@ -29,16 +29,26 @@ xhr.onload = function () {
     }
     clearPharmacyData();
     townF(this.value);
+    var dataList = document.getElementById('dataList');
+    var pagination = document.querySelector('.pagination');
+    dataList.textContent = "未選擇區鄉鎮";
+    pagination.style.display = 'none';
+
+
   });
   // 事件 -> 區鄉鎮 所選值改變時，連帶變更資料
   town.addEventListener('change', function (e) {
     townName = this.value;
     data = [];
-    // quantity.classList.add('d-inline');
     clearPharmacyData();
-    quantityMask(townName,quantityRecord)
-    
-    pharmacyList();
+    quantityMask(townName, quantityRecord);
+    pageLocalArray[0] = 1;
+    localStorage.setItem('pageLinkNumber', JSON.stringify(pageLocalArray));
+    pharmacyList(1);
+    var pagination = document.querySelector('.pagination');
+    pagination.style.display = 'block';
+
+
   });
 
   // 事件 -> 數量排序 所選值改變時，連帶變更資料
@@ -46,11 +56,13 @@ xhr.onload = function () {
 
     data = [];
     // console.log(typeof(this.value)); -> 類型為字串
-    quantityMask(townName,parseInt(this.value));
+    quantityMask(townName, parseInt(this.value));
     quantityRecord = parseInt(this.value);
-    
+
     clearPharmacyData();
-    pharmacyList();
+    pageLocalArray[0] = 1;
+    localStorage.setItem('pageLinkNumber', JSON.stringify(pageLocalArray));
+    pharmacyList(1);
     distance.checked = false;
   });
 
@@ -60,108 +72,142 @@ xhr.onload = function () {
     console.log(this.checked);
     if (this.checked == true) {
       data = [];
-      quantityMask(townName,quantityRecord);
-      
+      quantityMask(townName, quantityRecord);
       clearPharmacyData();
-      pharmacyList();
+      pageLocalArray[0] = 1;
+      localStorage.setItem('pageLinkNumber', JSON.stringify(pageLocalArray));
+      pharmacyList(1);
     } else {
       data = [];
-      quantityMask(townName,quantityRecord);
-      
+      quantityMask(townName, quantityRecord);
       clearPharmacyData();
-      pharmacyList();
+      pageLocalArray[0] = 1;
+      localStorage.setItem('pageLinkNumber', JSON.stringify(pageLocalArray));
+      pharmacyList(1);
+    }
+  });
+  // 事件 -> 點擊上下頁
+  var pageLocalArray = [1];
+  pageLocalArray[0] = 1;
+  localStorage.setItem('pageLinkNumber', JSON.stringify(pageLocalArray));
+  var pageLink = document.querySelectorAll('.page-link');
+  pageLink[0].addEventListener('click', function () {
+    if (pageLocalArray[0] - 1 === 0) {
+      alert('已經在第一頁 !')
+      return;
+    } else {
+      pageLocalArray[0] = pageLocalArray[0] - 1;
+      localStorage.setItem('pageLinkNumber', JSON.stringify(pageLocalArray));
+      clearPharmacyData();
+      pharmacyList(pageLocalArray[0]);
+      topFunction(0);
+    }
+
+  });
+  pageLink[1].addEventListener('click', function () {
+
+    if (pageLocalArray[0] + 1 === Math.ceil(data.length / 10) + 1) {
+      alert('最後一頁囉 !')
+      return;
+    } else {
+      pageLocalArray[0] = pageLocalArray[0] + 1;
+      localStorage.setItem('pageLinkNumber', JSON.stringify(pageLocalArray));
+      clearPharmacyData();
+      pharmacyList(pageLocalArray[0]);
+      topFunction(0);
     }
   });
 
 
-
   // 功能 -> 藥局清單
-  function pharmacyList() {
+  function pharmacyList(num) {
     var pharmacyRow = document.querySelector('.js-pharmacyList');
-    var filterData = [];
-    // var lastLength = '';
-    // var pageTotal = Math.ceil(pageTotalArray.length / 10);
-    // // TODO: 可新增 5筆、15筆、20筆資料
-    // // - 小於 10 筆資料
-    // // - 滿 10 筆資料且不是最後一頁
-    // // - 最後一頁且有餘數
-    // if (pageTotalArray.length < 10) {
-    //   lastLength = pageTotalArray.length % 10;
-    // } else if (pageTotalArray.length % 10 === 0 || num !== pageTotal) {
-    //   lastLength = num * 10;
-    // } else if (pageTotalArray.length % 10 !== 0 && num === pageTotal) {
-    //   lastLength = (num - 1) * 10 + pageTotalArray.length % 10;
-    // }
-    // i = (num - 1) * 10; i < lastLength; i++
+    var dataList = document.getElementById('dataList');
 
-    for (i = 0; i < data.length; i++) {
-
-        console.log(data.length)
-
-        filterData.push(data[i]);
-        var col = document.createElement('div');
-        var card = document.createElement('div');
-        var flex = document.createElement('div');
-        var mask__Pharmacy = document.createElement('h4');
-        var mask__Adult = document.createElement('h6');
-        var mask__Child = document.createElement('h6');
-        var mask__Address = document.createElement('a');
-        var mask__Phone = document.createElement('a');
-        var mask__Updated = document.createElement('p');
-        var mask__Note = document.createElement('p');
-        var mask__Distance = document.createElement('small');
-
-        
-        mask__Pharmacy.textContent = data[i].properties.name;
-        mask__Adult.innerHTML = '成人' + '<br>' + data[i].properties.mask_adult;
-        mask__Child.innerHTML = '兒童' + '<br>' + data[i].properties.mask_child;
-        mask__Address.innerHTML = '<span class="material-icons mr-1">place</span>' + data[i].properties.address;
-        mask__Phone.innerHTML = '<span class="material-icons">phonelink_ring</span>' + data[i].properties.phone;
-        mask__Updated.innerHTML = '<span class="material-icons mr-1cd">update</span>' + data[i].properties.updated;
-        mask__Note.innerHTML = '<span class="material-icons">event_note</span>' + data[i].properties.note;
-        mask__Distance.textContent = data[i].geometry.distance + 'km';
-
-        col.classList.add('col', 'col-md-4', 'col-lg-12', 'mb-3', 'js-col');
-        card.classList.add('card', 'p-1');
-        flex.classList.add('d-flex', 'justify-content-around');
-        mask__Pharmacy.classList.add('text-center', 'font-weight-bold', 'mt-2', 'mr-2');
-        mask__Distance.classList.add('ml-2');
-        mask__Address.classList.add('d-flex', 'align-items-start');
-        mask__Address.setAttribute('href', 'http://maps.google.com/maps?q=' + data[i].properties.address);
-        mask__Phone.classList.add('d-flex', 'align-items-center', 'ml-1', 'my-2');
-        mask__Phone.setAttribute('href', 'tel:' + data[i].properties.phone);
-        mask__Updated.classList.add('d-flex', 'align-items-center');
-        mask__Note.classList.add('d-flex', 'align-items-start');
+    var dataLength = '';
+    var pageTotal = Math.ceil(data.length / 10);
+    dataList.textContent = '共' + data.length + '筆資料，第' + num + '頁，共' + pageTotal + '頁'
+    // TODO: 可新增 5筆、15筆、20筆資料
+    // - 小於 10 筆資料
+    // - 滿 10 筆資料且不是最後一頁
+    // - 最後一頁且有餘數
+    if (data.length < 10) {
+      dataLength = data.length % 10;
+    } else if (data.length % 10 === 0 || num !== pageTotal) {
+      dataLength = num * 10;
+    } else if (data.length % 10 !== 0 && num === pageTotal) {
+      dataLength = (num - 1) * 10 + data.length % 10;
+    }
+    // i = 0; i < data.length; i++
+    // i = (num - 1) * 10; i < dataLength; i++
 
 
-        mask__Adult.classList.add('p-3', 'h3', 'text-white', 'rounded', 'text-nowrap', 'mr-1');
-        mask__Child.classList.add('p-3', 'h3', 'text-white', 'rounded', 'text-nowrap');
-        if (data[i].properties.mask_adult === 0) {
-          mask__Adult.classList.add('bg-zero', 'mask__quantity', 'mask__zero')
-        } else if (data[i].properties.mask_adult <= 50) {
-          mask__Adult.classList.add('bg-warning', 'mask__quantity', 'mask__warning')
-        } else if (data[i].properties.mask_adult > 50) {
-          mask__Adult.classList.add('bg-success', 'mask__quantity', 'mask__success')
-        }
-        if (data[i].properties.mask_child === 0) {
-          mask__Child.classList.add('bg-zero', 'mask__quantity', 'mask__zero')
-        } else if (data[i].properties.mask_child <= 50) {
-          mask__Child.classList.add('bg-warning', 'mask__quantity', 'mask__warning')
-        } else if (data[i].properties.mask_child > 50) {
-          mask__Child.classList.add('bg-success', 'mask__quantity', 'mask__success')
-        }
+
+    for (i = (num - 1) * 10; i < dataLength; i++) {
+
+      var col = document.createElement('div');
+      var card = document.createElement('div');
+      var flex = document.createElement('div');
+      var mask__Pharmacy = document.createElement('h4');
+      var mask__Adult = document.createElement('h6');
+      var mask__Child = document.createElement('h6');
+      var mask__Address = document.createElement('a');
+      var mask__Phone = document.createElement('a');
+      var mask__Updated = document.createElement('p');
+      var mask__Note = document.createElement('p');
+      var mask__Distance = document.createElement('small');
 
 
-        pharmacyRow.appendChild(col).appendChild(card);
-        card.appendChild(flex);
-        flex.appendChild(mask__Adult);
-        flex.appendChild(mask__Child);
-        card.appendChild(mask__Pharmacy).appendChild(mask__Distance);
-        card.appendChild(mask__Address);
-        card.appendChild(mask__Phone);
-        card.appendChild(mask__Updated);
-        card.appendChild(mask__Note);
-      
+      mask__Pharmacy.textContent = data[i].properties.name;
+      mask__Adult.innerHTML = '成人' + '<br>' + data[i].properties.mask_adult;
+      mask__Child.innerHTML = '兒童' + '<br>' + data[i].properties.mask_child;
+      mask__Address.innerHTML = '<span class="material-icons mr-1">place</span>' + data[i].properties.address;
+      mask__Phone.innerHTML = '<span class="material-icons">phonelink_ring</span>' + data[i].properties.phone;
+      mask__Updated.innerHTML = '<span class="material-icons mr-1cd">update</span>' + data[i].properties.updated;
+      mask__Note.innerHTML = '<span class="material-icons">event_note</span>' + data[i].properties.note;
+      mask__Distance.textContent = data[i].geometry.distance + 'km';
+
+      col.classList.add('col', 'col-md-4', 'col-lg-12', 'mb-3', 'js-col');
+      card.classList.add('card', 'p-1');
+      flex.classList.add('d-flex', 'justify-content-around');
+      mask__Pharmacy.classList.add('text-center', 'font-weight-bold', 'mt-2', 'mr-2');
+      mask__Distance.classList.add('ml-2');
+      mask__Address.classList.add('d-flex', 'align-items-start');
+      mask__Address.setAttribute('href', 'http://maps.google.com/maps?q=' + data[i].properties.address);
+      mask__Phone.classList.add('d-flex', 'align-items-center', 'ml-1', 'my-2');
+      mask__Phone.setAttribute('href', 'tel:' + data[i].properties.phone);
+      mask__Updated.classList.add('d-flex', 'align-items-center');
+      mask__Note.classList.add('d-flex', 'align-items-start');
+
+
+      mask__Adult.classList.add('p-3', 'h3', 'text-white', 'rounded', 'text-nowrap', 'mr-1');
+      mask__Child.classList.add('p-3', 'h3', 'text-white', 'rounded', 'text-nowrap');
+      if (data[i].properties.mask_adult === 0) {
+        mask__Adult.classList.add('bg-zero', 'mask__quantity', 'mask__zero')
+      } else if (data[i].properties.mask_adult <= 50) {
+        mask__Adult.classList.add('bg-warning', 'mask__quantity', 'mask__warning')
+      } else if (data[i].properties.mask_adult > 50) {
+        mask__Adult.classList.add('bg-success', 'mask__quantity', 'mask__success')
+      }
+      if (data[i].properties.mask_child === 0) {
+        mask__Child.classList.add('bg-zero', 'mask__quantity', 'mask__zero')
+      } else if (data[i].properties.mask_child <= 50) {
+        mask__Child.classList.add('bg-warning', 'mask__quantity', 'mask__warning')
+      } else if (data[i].properties.mask_child > 50) {
+        mask__Child.classList.add('bg-success', 'mask__quantity', 'mask__success')
+      }
+
+
+      pharmacyRow.appendChild(col).appendChild(card);
+      card.appendChild(flex);
+      flex.appendChild(mask__Adult);
+      flex.appendChild(mask__Child);
+      card.appendChild(mask__Pharmacy).appendChild(mask__Distance);
+      card.appendChild(mask__Address);
+      card.appendChild(mask__Phone);
+      card.appendChild(mask__Updated);
+      card.appendChild(mask__Note);
+
     }
 
   }
@@ -209,12 +255,12 @@ xhr.onload = function () {
         data.push(originalData[i]);
       }
     }
-    if (distance.checked === true){
+    if (distance.checked === true) {
       data.sort(function (a, b) {
         return a.geometry.distance > b.geometry.distance ? 1 : -1;
       });
       console.log(data)
-    }else {
+    } else {
       data.sort(function (a, b) {
         return a.properties.mask_adult > b.properties.mask_adult ? -1 : 1;
       });
@@ -242,8 +288,8 @@ xhr.onload = function () {
       townName = originalData[0].properties.town;
 
       // 所在區域藥局清單，顯示第一頁
-      quantityMask(townName,0)
-      pharmacyList();
+      quantityMask(townName, 0)
+      pharmacyList(1);
 
 
 
